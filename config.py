50023f03d7c36f6982e53e1854323fa959edb3ae
Alok963-ai <alokjangde6@gmail.com>
2025-06-17 11:45:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "26572696"))
-API_HASH = environ.get("API_HASH", "67a8947a3e1b15f9ef9684286baa34cb")
+API_ID = int(environ.get("API_ID", "20288951"))
+API_HASH = environ.get("API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-AUTH_USER = os.environ.get('AUTH_USERS', '7903596276').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7833842279').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 HOST = "https://drm-api-six.vercel.app"
-CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+CREDIT = "kaka"
 ADMIN_ID = [7833842279]
-DB_URL = "mongodb+srv://alokjangde"
+DB_URL = "mongodb+srv://alokjangde6:Sapnaipali71$@cluster0.cx60pkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL = -1002548493129 # Your Log Channel ID (Bot ko ADMIN BNAYE)
 USERLINK = "t.me/Alliswell71"
 #WEBHOOK = True  # Don't change this
